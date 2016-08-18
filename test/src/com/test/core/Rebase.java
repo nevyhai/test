e5c70d7bb0c5e015b68b9f1c1a4f22b25ae943d9
nevyhai <119525530@qq.com>
2016-08-18 14:59:38 +0800

    fdas

--- test/src/com/test/core/Rebase.java
+++ test/src/com/test/core/Rebase.java
@@ -1,5 +1,8 @@
 package com.test.core;
 
 public class Rebase {
-
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("1111111=======111111111");
+	}
 }
